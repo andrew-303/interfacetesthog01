2094b3b625f42d69fea2045f00dea00158ba188f
andrew-303 <a_biyanliang@126.com>
2020-01-07 21:25:50 +0800

    创建成员测试相关类--修改成员--其中加入了传入HashMap作为参数的逻辑

--- src/test/java/com/hogwarz/interfacetest/api/User.java
+++ src/test/java/com/hogwarz/interfacetest/api/User.java
@@ -3,6 +3,8 @@ package com.hogwarz.interfacetest.api;
 import com.hogwarz.interfacetest.Work;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
 
 /**
@@ -19,6 +21,23 @@ public class User {
                 .queryParam("userid",userId)
                 .when().log().all()
                 .get("https://qyapi.weixin.qq.com/cgi-bin/user/get")
+                .then().log().all()
+                .extract().response();
+    }
+
+    /**
+     * 更新成员信息
+     * 开始传入复杂参数数据,如HashMap
+     */
+    public Response update(String userId, HashMap<String, Object> data) {
+        //传的参数内容
+        data.put("userid",userId);
+
+        return given()
+                .queryParam("access_token",Work.getInstance().getToken())
+                .body(data)
+                .when().log().all()
+                .post("https://qyapi.weixin.qq.com/cgi-bin/user/update")
                 .then().log().all()
                 .extract().response();
     }
--- src/test/java/com/hogwarz/interfacetest/testcase/TestUser.java
+++ src/test/java/com/hogwarz/interfacetest/testcase/TestUser.java
@@ -3,6 +3,9 @@ package com.hogwarz.interfacetest.testcase;
 import com.hogwarz.interfacetest.api.User;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.equalTo;
 
 public class TestUser {
@@ -15,5 +18,22 @@ public class TestUser {
     public void getInfo() {
         String userId = "111111";
         user.getInfo(userId).then().body("name",equalTo("1111111"));
+    }
+
+    /**
+     * 更新用户信息
+     */
+    @Test
+    public void update() {
+        User user = new User();
+        String userId = "111111";
+        String nameNew = "修改的名字";
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("userid",userId);
+        data.put("name",nameNew);
+        user.update(userId,data);
+        user.getInfo(userId).then().body("name",equalTo(nameNew));
+
+
     }
 }
